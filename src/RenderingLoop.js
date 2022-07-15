a92c889de1aecf34cd2b13db29b80e71ae7e101a
i3ernie <mail@javaner.de>
2022-07-15 04:58:20 +0200

    class vs prototype

--- src/RenderingLoop.js
+++ src/RenderingLoop.js
@@ -3,58 +3,56 @@ import Loop from "./Loop.js";
 //////////////////////////////////////////////////////////////////////////////////
 //		THREEx.RenderingLoop						//
 //////////////////////////////////////////////////////////////////////////////////
-const RenderingLoop = function( renderer )
-{
-    Loop.call( this );
+class RenderingLoop extends Loop {
+    constructor ( renderer )
+    {
+        super();
 
-    this.renderer = renderer;
+        this.renderer = renderer;
 
-    this.maxDelta	= 0.2;	
-    //////////////////////////////////////////////////////////////////////////////////
-    //		start/stop/isRunning functions					//
-    //////////////////////////////////////////////////////////////////////////////////
+        this.maxDelta	= 0.2;	
+        //////////////////////////////////////////////////////////////////////////////////
+        //		start/stop/isRunning functions					//
+        //////////////////////////////////////////////////////////////////////////////////
     
-    /**
-     * 
-     * @returns {undefined}
-     */
-    this.start = function( obj ){
-        console.assert( this.isRunning === false );
+        /**
+         * 
+         * @returns {undefined}
+         */
+        this.start = function( obj ) {
+            console.assert( this.isRunning === false );
 
-        const scope = this;
-        this.add(function(){
-            scope.renderer.render( obj.scene, obj.camera );
-        });
-        this.renderer.setAnimationLoop( this.animation );
-        this.isRunning = true;
-        
-        return this;
-    };
-    
-    /**
-     * 
-     * @returns {Boolean}
-     */
-    this.isRunning = function(){
-        return !!this.isRunning;
-    };
-    
-    /**
-     * 
-     * @returns {undefined}
-     */
-    this.stop = function() {
-
-        if( !this.isRunning ) { 
-            return
+            const scope = this;
+            this.add(function(){
+                scope.renderer.render( obj.scene, obj.camera );
+            });
+            this.renderer.setAnimationLoop( this.animation );
+            this.isRunning = true;
+            
+            return this;
         };
-        this.renderer.setAnimationLoop( null );
-        this.isRunning = false;
-    };
-};
+    
+        /**
+         * 
+         * @returns {Boolean}
+         */
+        this.isRunning = function() {
+            return !!this.isRunning;
+        };
+    
+        /**
+         * 
+         * @returns {undefined}
+         */
+        this.stop = function() {
 
-RenderingLoop.prototype = Object.assign( Object.create( Loop.prototype ), {
-    constructor : RenderingLoop
-});
+            if( !this.isRunning ) { 
+                return
+            };
+            this.renderer.setAnimationLoop( null );
+            this.isRunning = false;
+        };
+    }
+};
 
 export {RenderingLoop, Loop };
