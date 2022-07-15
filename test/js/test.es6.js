6a14035a06e680c88d98289e1d2f2034968d0c6a
i3ernie <mail@javaner.de>
2022-07-15 05:09:48 +0200

    niceprint

--- test/js/test.es6.js
+++ test/js/test.es6.js
@@ -1,4 +1,4 @@
-import * as THREE from "../../node_modules/three/build/three.module.js";
+import * as THREE from "three";
 import { RenderingLoop } from "../../src/RenderingLoop.js";
 
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
