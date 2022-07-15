136e636b2f2431efa867007b78376a0da8b1feba
i3ernie <mail@javaner.de>
2022-07-15 04:58:50 +0200

    niceprint

--- test/js/test.es6.js
+++ test/js/test.es6.js
@@ -1,34 +1,34 @@
 import * as THREE from "../../node_modules/three/build/three.module.js";
-import {Loop, RenderingLoop} from "../../src/RenderingLoop.js";
+import { RenderingLoop } from "../../src/RenderingLoop.js";
 
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
-
-
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 const loop = new RenderingLoop( renderer ).start({scene:scene, camera:camera});
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const mesh = new THREE.Mesh( geometry, material );
+const mesh = new THREE.Mesh ( 
+    new THREE.BoxGeometry( 0.2, 0.2, 0.2 ), 
+    new THREE.MeshNormalMaterial() 
+);
+
 // animation
-const ani = function ( time ) {
+mesh.animation = function ( time ) 
+{
+	this.rotation.x = time / 2000;
+	this.rotation.y = time / 1000;
+}.bind(mesh);
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-};
 mesh.addEventListener("added", function(){
-    loop.add(ani);
+    loop.add( mesh.animation );
 });
 mesh.addEventListener("removed", function(){
-    loop.remove(ani);
+    loop.remove( mesh.animation );
 });
-scene.add( mesh );
 
+scene.add( mesh );
